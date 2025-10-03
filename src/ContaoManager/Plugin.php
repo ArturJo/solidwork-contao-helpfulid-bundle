525b22176c9b737ef4c631258237631a585c0ae7
Artur Jozefiak <jozefiak@secra.de>
2025-10-03 15:08:13 +0100

    Add ContaoHelpfulidBundle with service configuration and Contao Manager plugin

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,20 @@
+<?php
+// src/ContaoManager/Plugin.php
+namespace SolidWork\ContaoHelpfulidBundle\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\CoreBundle\ContaoCoreBundle;
+use SolidWork\ContaoHelpfulidBundle\ContaoHelpfulidBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(ContaoExampleBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
